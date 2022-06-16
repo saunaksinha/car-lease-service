1f8bbd9ddb4e1262d001f5e39bb0b0cdf2b63d45
sinhasa <saunak.sinha@rabobank.nl>
2022-06-16 10:23:38 +0200

    Updated the constants.

--- src/main/java/com/assignment/car/lease/common/ApplicationConstants.java
+++ src/main/java/com/assignment/car/lease/common/ApplicationConstants.java
@@ -11,13 +11,5 @@ public final class ApplicationConstants {
     private ApplicationConstants() {
         // restrict instantiation
     }
-
-    public static final String SUCCESSFUL = "SUCCESSFUL";
-    public static final String DUPLICATE_REFERENCE = "DUPLICATE_REFERENCE";
-    public static final String BAD_REQUEST = "Invalid format of customer data received";
-    public static final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
-
     public static final String CUSTOMER_IS_NULL = "Customer is null";
-    public static final String CUSTOMER_DOB_IS_NULL = "Invalid customer object received, missing date of birth";
-    public static final String ERROR_IN_PROCESSING_AGE = "Pension Age can not be calculated";
 }
